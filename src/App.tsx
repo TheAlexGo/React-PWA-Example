235b97f4efdf56a6a828d5663f4f5c2751acfb8f
Александр <alexander.gordeev@vk.team>
2022-03-24 02:56:46 +0300

    Cleaning

--- src/App.tsx
+++ src/App.tsx
@@ -9,27 +9,6 @@ const App = () => {
             <Suspense fallback={<div />}>
                 <div className="App">
                     <Routes>
-                        {/*<Route path="/" element={<Layouts.Main />}>*/}
-                        {/*<Route index element={<GeneralPage />} />*/}
-                        {/*<Route path={LINKS.LIBRARY_LINK} element={<LibraryPage />} />*/}
-                        {/*<Route path={`${LINKS.LIBRARY_LINK}/:mangaId`}>*/}
-                        {/*    <Route index element={<MangaPage />} />*/}
-                        {/*    <Route path={`${LINKS.READER_LINK}/:chapterId`} element={<ReaderPage />} />*/}
-                        {/*</Route>*/}
-
-                        {/*<Route path={LINKS.PROFILE_LINK}>*/}
-                        {/*    <Route index element={<ProfilePage />} />*/}
-                        {/*    <Route path={LINKS.LOGIN_LINK} element={<SignInPage />} />*/}
-                        {/*    <Route path={LINKS.REGISTER_LINK} element={<RegisterPage />} />*/}
-                        {/*    <Route path={LINKS.RECOVERY_LINK}>*/}
-                        {/*        <Route index element={<RecoveryPage />} />*/}
-                        {/*        <Route*/}
-                        {/*            path={`${LINKS.RECOVERY_SUCCESS_LINK}/:mail`}*/}
-                        {/*            element={<RecoverySuccessPage />}*/}
-                        {/*        />*/}
-                        {/*    </Route>*/}
-                        {/*</Route>*/}
-                        {/*</Route>*/}
                         <Route path="*" element={<Page404 />} />
                     </Routes>
                 </div>
