45f3cda77cf3b4d552fc5a4642e7b26b59951d37
Александр <alexander.gordeev@vk.team>
2022-03-27 14:46:02 +0300

    Update sb config. Add fonts. Add common style

--- src/config/aliases.js
+++ src/config/aliases.js
@@ -4,11 +4,14 @@ const aliases = (prefix = `src`) => ({
     '@config': `${prefix}/config`,
     '@hooks': `${prefix}/hooks`,
     '@assets': `${prefix}/assets`,
+    '@images': `${prefix}/assets/images`,
+    '@fonts': `${prefix}/assets/fonts`,
     '@utils': `${prefix}/utils`,
     '@services': `${prefix}/services`,
     '@types': `${prefix}/types`,
     '@store': `${prefix}/store`,
     '@languages': `${prefix}/languages`,
+    '@styles': `${prefix}/styles`,
     'bem-cn-custom': `${prefix}/utils/bemCnCustom`
 });
 
